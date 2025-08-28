bd718d74fc3e1906deefea126ac918cda5167142
RodrigoAlban <rodrigoalban@alunos.utfpr.edu.br>
2025-08-28 11:44:14 -0300

    Implementação inicial da atividade

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+string nome = "Alex";
+int idade = 18;
+char inicialNome = 'A';
+float salario = 1500;
+
+Console.WriteLine($"Nome: {nome}");
+Console.WriteLine($"Idade: {idade}");
+Console.WriteLine($"Inicial do Nome: {inicialNome}");
+Console.WriteLine($"Salário: {salario}");
